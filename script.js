a671f481a93d56120879b2f273f469fa32cfd1f3
Ebert Zerpa <ebertunerg@gmail.com>
2024-02-15 21:37:47 -0500

    Función Calcular

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var resultado = "0";
+var temp = 0;
+var operacion = "";
 
 function alertaPrueba() {
   alert("Hola Mundo");
 }
 
 function add(texto) {
-  console.log(typeof resultado)
+  texto = +texto;
   if (resultado == 0) {
     resultado = '';
   }
@@ -17,7 +19,45 @@ function pintaResultado() {
   document.getElementById("resultado").value = resultado;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+function limpiar() {
+  resultado = 0;
+  temp = 0;
+  pintaResultado();
+}
 
+function setOperacion(op) {
+  operacion = op;
+  temp = resultado;
+  resultado = 0;
+  pintaResultado();
+}
+
+function calcular() {
+  switch (operacion) {
+    case "+":
+      resultado = parseFloat(temp) + parseFloat(resultado);
+      break;
+    case "-":
+      resultado = parseFloat(temp) - parseFloat(resultado);
+      break;
+  }
+  pintaResultado();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("btn4").addEventListener("click", () => { add(4); })
+  document.getElementById("btn5").addEventListener("click", () => { add(5); })
+  document.getElementById("btn6").addEventListener("click", () => { add(6); })
+
+  // document.getElementById("btn1").addEventListener("mouseover", () => { alert('') })
+
+  for (let i = 0; i < 4; i++) {
+    document.getElementById(`btn${i}`).addEventListener("click", () => { add(i); })
+  }
+
+  document.getElementById("btnClear").addEventListener("click", () => { limpiar(); })
+
+
+  console.log(document.getElementById("btn5"))
 });
 
